fd052470c2c4697eb6d6069adfb566fa2f4f58b9
Dan Hono <stelveflame@gmail.com>
2024-03-27 03:08:01 +0400

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let divisibleByFour = year % 4 === 0;
+    if (divisibleByFour && year % 100 !== 0) {
+        return true;
+    } else if (divisibleByFour && year % 400 === 0) {
+        return true;
+    }
+    return false;
 };
+
+// A better solution, really:
+// return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 
 // Do not edit below this line
 module.exports = leapYears;
