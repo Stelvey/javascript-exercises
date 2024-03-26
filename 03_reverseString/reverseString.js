f7e757cfe3711ac877c83d913f1be1bbd88e6e30
Dan Hono <stelveflame@gmail.com>
2024-03-27 01:18:13 +0400

    Complete 03_reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let charsArray = text.split('');
+    charsArray.reverse();
+    return charsArray.join('');
 };
 
 // Do not edit below this line
